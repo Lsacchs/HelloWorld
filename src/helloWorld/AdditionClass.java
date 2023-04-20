ac67ff682a371470f58154cd404c5afd79f58088
Lsacchs <lama.sachs@gmail.com>
2023-04-19 23:01:37 -0400

    Second Commit

---
+++ src/helloWorld/AdditionClass.java
@@ -0,0 +1,21 @@
+package helloWorld;
+
+import java.util.Scanner;
+
+public class AdditionClass {
+
+	public static void main(String[] args) {
+		 int x, y, sum;
+		    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		    System.out.println("Type a number:");
+		    x = myObj.nextInt(); // Read user input
+
+		    System.out.println("Type another number:");
+		    y = myObj.nextInt(); // Read user input
+
+		    sum = x + y;
+		    System.out.println("Sum is: " + sum); // Output user input
+
+	}
+
+}
