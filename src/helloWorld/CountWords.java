fd59bbe189c7adac2d87f3c3cd913dba9ef3a51b
Lsacchs <lama.sachs@gmail.com>
2023-04-19 23:10:50 -0400

    Fifth Commit

---
+++ src/helloWorld/CountWords.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class CountWords {
+
+	public static void main(String[] args) {
+		String words = "One Two Three Four";
+	    int countWords = words.split("\\s").length;
+	    System.out.println(countWords);
+	}
+
+}
