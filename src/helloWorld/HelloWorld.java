f7e81cc7716b8af1047a5adcdd8738350bd82f87
Lsacchs <lama.sachs@gmail.com>
2025-07-15 21:36:39 -0400

    config user test

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("New first project");
 		System.out.println("New line added");
+		System.out.println("config tested");
 	}
 
 }
